e33136e352008a36e41e754cfd6ed472207cd8ea
Matthew Downs <matthew.downsc@gmail.com>
2026-02-01 20:16:26 -0600

    comment out patch

--- AlterraWeaponry/Patches/IngameMenu_SaveGameAsync.cs
+++ AlterraWeaponry/Patches/IngameMenu_SaveGameAsync.cs
@@ -1,17 +1,17 @@
-using VELD.AlterraWeaponry.Mono.DepthCharge;
+// using VELD.AlterraWeaponry.Mono.DepthCharge;
 
-namespace VELD.AlterraWeaponry.Patches;
+// namespace VELD.AlterraWeaponry.Patches;
 
-/// <summary>
-/// Patches IngameMenu.SaveGameAsync to save air bladder state when the player saves the game.
-/// </summary>
-[HarmonyPatch(typeof(IngameMenu), "SaveGameAsync")]
-public class SaveGameAsync_Patch
-{
-    [HarmonyPrefix]
-    public static void SaveGameAsync_Prefix()
-    {
-        // Update and save all depth charge states before the game is saved
-        DepthChargeStateManager.Save();
-    }
-}
+// /// <summary>
+// /// Patches IngameMenu.SaveGameAsync to save air bladder state when the player saves the game.
+// /// </summary>
+// [HarmonyPatch(typeof(IngameMenu), "SaveGameAsync")]
+// public class SaveGameAsync_Patch
+// {
+//     [HarmonyPrefix]
+//     public static void SaveGameAsync_Prefix()
+//     {
+//         // Update and save all depth charge states before the game is saved
+//         DepthChargeStateManager.Save();
+//     }
+// }
